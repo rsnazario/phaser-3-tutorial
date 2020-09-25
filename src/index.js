15f50783537b83ecd0ee724e12d926ad555e65e3
rsnazario <rafael.s.nazario@gmail.com>
2020-09-25 15:32:08 -0300

    Part 6 - Collision

--- src/index.js
+++ src/index.js
@@ -19,6 +19,8 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+let platforms;
+let player;
 
 function preload () {
   console.log('yes');
@@ -29,17 +31,45 @@ function preload () {
   this.load.spritesheet('dude', '../assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
-let platforms;
 
 function create () {
+  
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
   this.add.image(400, 300, 'star').setOrigin(0, 0);
-
+  
+  // platforms --> physics group --> Static --> wont move ever!
   platforms = this.physics.add.staticGroup();
   platforms.create(400, 568, 'ground').setScale(2).refreshBody();
   platforms.create(600, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
+
+  // player declaration
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create( {
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+    frameRate: 10,
+    repeat: -1
+  });
+  this.anims.create({
+    key: 'turn',
+    frames: [ {key: 'dude', frame: 4} ],
+    frameRate: 20,
+  });
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+    frameRate: 10,
+    repeat: -1
+  })
+
+  // player x platform collision:
+  this.physics.add.collider(player, platforms);
 }
 
 function update ()
